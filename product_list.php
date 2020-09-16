210aa26571c8b54a47783c74a08f72b16f8a27f4
poomphattara26742 <71369665+poomphattara26742@users.noreply.github.com>
2020-09-16 13:51:33 +0700

    Pilowy Project
    Version 1

---
+++ product_list.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="zxx">
+
+<?php
+require("head.html");
+require('connectDB.php');
+
+
+?>
+
+<body>
+<!--::header part start::-->
+<?php
+require("header.php");
+?>
+<!-- Header part end-->
+
+<!-- breadcrumb part start-->
+<section class="breadcrumb_part">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb_iner">
+                    <h2>รายการสินค้าของเรา</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- breadcrumb part end-->
+
+<!-- product list part start-->
+<section class="product_list section_padding pt-md-2 pt-2 pl-2 pr-2">
+<div class="container-fluid">
+
+<h3 align = 'left'>หมอนยางพารา Cotton 100%</h3>
+<img align ='right' class="img_icon" src="img/Menu_Vertical.png" width=40px alt="">
+
+  <div class="card-grid">
+  
+  <div class="card-pd">
+        <img class="img_product" src="img/single_product_2.webp" alt=""><br><br>
+        <div class="content">
+           <h5>Contour</h5>
+           <h5>ชื่อหมอน ก</h5>
+           <span>480 บาท</span><br><br>
+           <button type="button" class="btn btn-dark">สั่งซื้อสินค้า</button>
+      </div>
+     </div>
+
+     <div class="card-pd">
+        <img class="img_product" src="img/single_product_4.webp" alt=""><br><br>
+        <div class="content">
+           <h5>Heart/Durian</h5>
+           <h5>ชื่อหมอน ข</h5>
+           <span>600 บาท</span><br><br>
+           <button type="button" class="btn btn-dark">สั่งซื้อสินค้า</button>
+      </div>
+     </div>
+
+     <div class="card-pd">
+        <img class="img_product" src="img/single_product_5.webp" alt=""><br><br>
+        <div class="content">
+           <h5>Massage/Knobby</h5>
+           <h5>ชื่อหมอน ค</h5>
+           <span>480 บาท</span><br><br>
+           <button type="button" class="btn btn-dark">สั่งซื้อสินค้า</button>
+      </div>
+     </div>
+
+  </div>
+  </div>
+
+</section>
+<!-- product list part end-->
+
+<!--::footer_part start::-->
+<?php
+require("footer.php");
+?>
+<!--::footer_part end::-->
+
+<!-- jquery plugins here-->
+<script src="js/jquery-1.12.1.min.js"></script>
+<!-- popper js -->
+<script src="js/popper.min.js"></script>
+<!-- bootstrap js -->
+<script src="js/bootstrap.min.js"></script>
+<!-- easing js -->
+<script src="js/jquery.magnific-popup.js"></script>
+<!-- swiper js -->
+<script src="js/swiper.min.js"></script>
+<!-- swiper js -->
+<script src="js/mixitup.min.js"></script>
+<!-- particles js -->
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<!-- slick js -->
+<script src="js/slick.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/contact.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+<script src="js/jquery.form.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/mail-script.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
+</body>
+
+</html>
