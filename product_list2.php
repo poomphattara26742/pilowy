6ebf4674006de99eb681a4caaabf37e56f506a5a
poomphattara26742 <71369665+poomphattara26742@users.noreply.github.com>
2020-09-18 16:23:41 +0700

    Poom Upload 18/9/2563 16.22 PM
    ทำหน้า รายการสินค้า รายละเอียดสินค้า (หน้าเเปล่าๆ ยังไม่ใส่ Database)

--- product_list2.php
+++ product_list2.php
@@ -54,6 +54,7 @@ require("header.php");
               <path d="M0 3.5A.5.5 0 0 1 .5 3h15a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-2zm0 5A.5.5 0 0 1 .5 8h15a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1zm0 4a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"/>
                </svg>
             </button>
+            
 </div>
 
 
@@ -69,7 +70,7 @@ require("header.php");
 
 
 <div class="p-2">
-     <button class="btn btn-outline-dark ml-auto mr-auto dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ราคา</button>
+     <button class="btn_sortby dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ราคา</button>
    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
       <a class="dropdown-item" href="#">ราคาถูก-ราคาแพง</a>
       <a class="dropdown-item" href="#">ราคาแพง-ราคาถูก</a>
